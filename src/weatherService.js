e74272d8ba717611b401bf1b9429fba89f266c4e
Piyush Anand <anandpiyush0524@gmail.com>
2023-03-20 11:13:09 +0530

    Initial commit

---
+++ src/weatherService.js
@@ -0,0 +1,37 @@
+const API_KEY = "b203a15bbf9051a7a3459a8cc20e4a26";
+
+const makeIconURL = (iconID) => `http://openweathermap.org/img/wn/${iconID}@2x.png`;
+
+const getformattedWeatherData = async (city, units = "metric") => {
+  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=${units}`;
+
+  const data = await fetch(URL)
+    .then((res) => res.json())
+    .then((data) => data);
+
+  const {
+    weather,
+    main: { temp, feels_like, temp_min, temp_max, pressure, humidity },
+    wind: { speed },
+    sys: { country },
+    name,
+  } = data;
+
+  const { description, icon } = weather[0];
+
+  return {
+    description,
+    iconURL: makeIconURL(icon),
+    temp,
+    feels_like,
+    temp_min,
+    temp_max,
+    pressure,
+    humidity,
+    speed,
+    country,
+    name,
+  };
+};
+
+export { getformattedWeatherData };
